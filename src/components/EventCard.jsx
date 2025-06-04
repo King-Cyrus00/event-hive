33aa14a37e74b8c84d6683c4b19161c89157574a
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-04 16:40:11 +0100

    images updated

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,9 +1,9 @@
 import eventCardImage from "../assets/images/event-card-image.jpg";
 
-export default function EventCard( ) {
+export default function EventCard( {event}) {
     return(
         <div>
-            <img src={eventCardImage} alt="Event Card"/>
+            <img src={event.image} alt="Event Card"/>
             <h1>Besteller Book Bootcamp -write, Market & Publish Your Book -Lucknow</h1>
             <h2>Saturday, March 18, 9.30</h2>
             <h3>Online Event- Attend anywhere</h3>
